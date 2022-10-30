6535453a9caa472074c0733eb2b518d458d6cdaf
Zion-W9 <zion_wang@hotmail.com>
2022-10-30 23:27:21 +0100

    Bob

--- Week5/Week5.py
+++ Week5/Week5.py
@@ -6,13 +6,9 @@ df = pd.DataFrame(np.random.randint(0,100,size=(1000000, 4)))
 
 df = pd.concat([df, df[3]], axis=1, ignore_index=True)
 
-print(df)
-
 # Dump to HDF
-store = pd.HDFStore('store.h5')
 start1 =time.perf_counter()
-store['df']=df
-store.close()
+df.to_hdf("Homework5.h5", key="fx/spot")
 print('HDF time:')
 print(time.perf_counter()-start1)
 
@@ -21,7 +17,7 @@ print(time.perf_counter()-start1)
 import pyarrow.feather as feather
 start2 =time.perf_counter()
 
-feather.write_feather(df,'Feather')
+feather.write_feather(df,'Homework5.arrow')
 print('Feather time:')
 print(time.perf_counter()-start2)
 
